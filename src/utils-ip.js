864e540b2f74f919af47d7371037acd202fed86a
bosscheng1210 <25022549@qq.com>
2020-04-23 15:10:46 +0800

    Create utils-ip.js

---
+++ src/utils-ip.js
@@ -0,0 +1,29 @@
+/**
+ * Date:2020/4/23
+ * Desc:
+ */
+const os = require('os');
+
+let _ip = ''; //
+
+module.exports = () => {
+    let result = '';
+    if (_ip) {
+        return _ip;
+    }
+
+    let interfaces = os.networkInterfaces();
+    for (let devName in interfaces) {
+        let iface = interfaces[devName];
+        for (let i = 0; i < iface.length; i++) {
+            let alias = iface[i];
+            if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
+                result = alias.address;
+            }
+        }
+    }
+
+    _ip = result;
+
+    return _ip;
+};
